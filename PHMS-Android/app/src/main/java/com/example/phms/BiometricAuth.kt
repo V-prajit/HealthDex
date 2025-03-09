c46b106424517c4aa99659a28835c563e1a2b5b9
CSCoderV <vsd8623@mavs.uta.edu>
2025-03-08 16:46:34 -0800

    getting started with biometric login

---
+++ PHMS-Android/app/src/main/java/com/example/phms/BiometricAuth.kt
@@ -0,0 +1,19 @@
+package com.example.phms
+
+import android.content.Context
+import android.hardware.biometrics.BiometricPrompt
+import android.os.CancellationSignal
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.biometric.BiometricManager
+import com.google.firebase.auth.FirebaseAuth
+import java.util.concurrent.Executor
+import androidx.core.content.ContextCompat
+
+class BiometricAuth(private val context:Context,private val authCallback: (Boolean)->Unit){
+    private val authentication= FirebaseAuth.getInstance //gets the current user logged into firebase
+    private var cancellationSignal: CancellationSignal? = null //lets user cancel bio authentication
+    private val executor: Executor = ContextCompat.getMainExecutor(context)
+    
+}
+
