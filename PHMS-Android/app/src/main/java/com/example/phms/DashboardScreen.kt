85f0972f9d107e90bcd855dcf1079874aacef80e
Ved Dharmatti <vsd8623@mavs.uta.edu>
2025-03-25 01:15:52 -0500

    Notes (#7)
    * added notes screen,basic dashboard
    
    * basic notes creation added
    
    * added basic notes functionality
    
    * basic notes implementation complete

---
+++ PHMS-Android/app/src/main/java/com/example/phms/DashboardScreen.kt
@@ -0,0 +1,50 @@
+package com.example.phms
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Note
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DashboardScreen(
+    firstName: String? = null,
+    onSettingsClick: () -> Unit = {}
+) {
+    var selectedTab by remember { mutableStateOf("home") }
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                //items on navigation bar: home & notes for now. todo add more features later(health,etc)
+                NavigationBarItem(
+                    selected = selectedTab == "home",
+                    onClick = { selectedTab = "home" },
+                    icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
+                    label = { Text("Home") }
+                )
+                NavigationBarItem(
+                    selected = selectedTab == "notes",
+                    onClick = { selectedTab = "notes" },
+                    icon = { Icon(Icons.Default.Note, contentDescription = "Notes") },
+                    label = { Text("Notes") }
+                )
+            }
+        }
+    ) { innerPadding ->
+        when (selectedTab) {
+            "home" -> HomeScreen(
+                firstName = firstName,
+                onSettingsClick = onSettingsClick
+            )
+            "notes" -> NotesScreen(modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
+@Composable
+fun NotesScreen(modifier: Modifier = Modifier) {
+    NotesFullApp()
+}
