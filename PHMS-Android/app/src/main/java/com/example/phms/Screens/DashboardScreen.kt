a56a349c047077cdb681084666a5fde61040e36a
Prajit Viswanadha <prajit.viswanadha.06@gmail.com>
2025-04-25 21:01:31 -0500

    Parital Working

--- PHMS-Android/app/src/main/java/com/example/phms/Screens/DashboardScreen.kt
+++ PHMS-Android/app/src/main/java/com/example/phms/Screens/DashboardScreen.kt
@@ -4,13 +4,23 @@ import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Chat
+import androidx.compose.material.icons.filled.EventNote
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MedicalServices
 import androidx.compose.material.icons.filled.Note
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Restaurant
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.phms.R
@@ -173,7 +183,7 @@ fun DashboardScreen(
                 onBackClick = { selectedTab = "home" },
                 onSettingsClick = { onSettingsClick("vitals") }
             )
-            "medications" -> MedicationsScreen(
+            "medications" -> PokemonMedicationsScreen(
                 userToken = userToken,
                 modifier = Modifier.padding(innerPadding),
                 onBack = { selectedTab = "home" }
